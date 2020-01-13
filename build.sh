8fa9369577d2a50f68e358bbaf72ec2178b21188
jackhaibo <625198232@qq.com>
2020-01-13 17:15:13 +0800

    fix bugs

--- build.sh
+++ build.sh
@@ -1,18 +1,26 @@
 #!/bin/bash
 
+pwd
 ls
+cd gateway && rm go.sum && rm go.mod
+cd run && CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -o gateway . && \
+    chmod +x gateway && docker build -t gateway:v1.0 .
+cd ../../
 
-cd gateway/run && docker build -t gateway:v1.0 .
-cd -
+cd item && rm go.sum && rm go.mod
+cd run && CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -o item . && \
+    chmod +x item && docker build -t item:v1.0 .
+cd ../../
 
-cd item/run && docker build -t item:v1.0 .
-cd -
+cd order && rm go.sum && rm go.mod
+cd run && CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -o order . && \
+    chmod +x order && docker build -t order:v1.0 .
+cd ../../
 
-cd order/run && docker build -t order:v1.0 .
-cd -
-
-cd user/run && docker build -t user:v1.0 .
-cd -
+cd user && rm go.sum && rm go.mod
+cd run && CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -o user . && \
+    chmod +x user && docker build -t user:v1.0 .
+cd ../../
 
 cd view && docker build -t view:v1.0 .
 cd -
